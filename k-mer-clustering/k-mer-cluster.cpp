39e6ccebe4f9899e16eb86c13768af6750bdbe90
haifeng.chen <chenhaifeng88888@gmail.com>
2017-03-21 20:24:41 -0700

    add k-mer clustering

---
+++ k-mer-clustering/k-mer-cluster.cpp
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <set>
+#include <limits>
+#include <vector>
+#include <string>
+#include <utility>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+#include "hierarchical_clustering_based_on_similarity_matrix.hpp"
+
+using clustering::HierarchicalClusteringBasedonSimilarityMatrix;
+
+using namespace std;
+
+int main() {
+  HierarchicalClusteringBasedonSimilarityMatrix hcs;
+
+  return 0;
+}
